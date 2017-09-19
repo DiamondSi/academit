1050086c7a5b1ec20acf2c826ce32ce2ffb240e4
DiamondSi <neighborland@outlook.com>
2017-09-19 07:01:35 +0700

    task Range created

---
+++ range/src/ru/academits/dubchak/range/Main.java
@@ -0,0 +1,13 @@
+package ru.academits.dubchak.range;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range = new Range(10, 20);
+        System.out.println(range.getLength());
+        System.out.println(range.isInside(15));
+        range.setFrom(15);
+        range.setTo(30);
+        System.out.println(range.getLength());
+        System.out.println(range.isInside(30));
+    }
+}
