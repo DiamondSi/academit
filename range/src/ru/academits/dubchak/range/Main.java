d433488184af6a3419e72a50ec74b545d80b48c4
DiamondSi <neighborland@outlook.com>
2017-09-28 12:37:23 +0700

    working on Range with a star

--- range/src/ru/academits/dubchak/range/Main.java
+++ range/src/ru/academits/dubchak/range/Main.java
@@ -7,17 +7,16 @@ public class Main {
         Range range = new Range(10, 20);
         System.out.printf("range [%f .. %f] length:%f%n", range.getFrom(), range.getTo(), range.getLength());
         System.out.printf("point 15 is inside: %b%n", range.isInside(15));
+
         range.setFrom(15);
         range.setTo(35);
         System.out.printf("range [%f .. %f] length:%f%n", range.getFrom(), range.getTo(), range.getLength());
         System.out.printf("point 30 is inside: %b%n", range.isInside(30));
-//        System.out.println(range.isInside(30));
+
         Range range1 = new Range(5, 30);
-//        System.out.println("range1: " + range1.getFrom() + ", " + range1.getTo());
         System.out.printf("range [%f .. %f] length:%f%n", range1.getFrom(), range1.getTo(), range1.getLength());
         Range range2 = new Range(20, 25);
         System.out.printf("range [%f .. %f] length:%f%n", range2.getFrom(), range2.getTo(), range2.getLength());
-//        System.out.println("range2: " + range2.getFrom() + ", " + range2.getTo());
 
         if (range1.getIntersection(range2) != null) {
             Range intersection = range1.getIntersection(range2);
@@ -26,15 +25,13 @@ public class Main {
             System.out.println("Intersection is: null");
         }
 
-        Range[] union = range1.getUnion(range2);
         System.out.println("Union is:");
-        for (Range r : union) {
+        for (Range r : range1.getUnion(range2)) {
             System.out.printf("%f %f%n", r.getFrom(), r.getTo());
         }
 
-        Range[] difference = range1.getDifference(range2);
         System.out.println("Difference is:");
-        for (Range r : difference) {
+        for (Range r : range1.getDifference(range2)) {
             System.out.printf("%f %f%n", r.getFrom(), r.getTo());
         }
     }
