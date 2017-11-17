3bf11fc7aa7b1db2a793dfa8d56beb78e27712e8
DiamondSi <neighborland@outlook.com>
2017-11-17 11:02:28 +0700

    list

---
+++ singlylinkedlist/src/ru/academits/dubchak/main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.dubchak.main;
+
+import ru.academits.dubchak.singlylinkedlist.SinglyLinkedList;
+
+public class Main {
+    SinglyLinkedList list = new SinglyLinkedList();
+}
--- singlylinkedlist/src/ru/academits/dubchak/singlylinkedlist/SinglyLinkedList.java
+++ singlylinkedlist/src/ru/academits/dubchak/singlylinkedlist/SinglyLinkedList.java
@@ -2,6 +2,34 @@ package ru.academits.dubchak.singlylinkedlist;
 
 public class SinglyLinkedList {
     private ListItem head;
+    private int length;
+
     public SinglyLinkedList() {
+        length = 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public ListItem getHead() {
+        return head;
+    }
+
+    public void insertItem(ListItem item) {
+        item.setNext(head);
+        head.setNext(item);
+    }
+
+    public ListItem getByIndex(int index) {
+        if (index >= length) {
+            return null;
+        } else {
+            ListItem currentItem = head;
+            for (int i = 0; i <= index; i++) {
+                currentItem = currentItem.getNext();
+            }
+            return currentItem;
+        }
     }
 }
