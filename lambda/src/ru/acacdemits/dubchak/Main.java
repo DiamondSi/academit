9790e2357c072fe004f315dcd9b33c95534bfa95
DiamondSi <neighborland@outlook.com>
2017-11-19 16:46:10 +0700

    codetime

---
+++ lambda/src/ru/acacdemits/dubchak/Main.java
@@ -0,0 +1,19 @@
+package ru.acacdemits.dubchak;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Person("Dima",44));
+        persons.add(new Person("Katya",41));
+        persons.add(new Person("Anna",38));
+        persons.add(new Person("Anna",14));
+        persons.stream()
+                .map(person-> person.getName())
+                .distinct()
+                .collect(Collectors.toList());
+                forEach(name-> System.out.println(name));
+    }
+}
