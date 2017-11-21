8b979f630677e582686c34164e468d42b1f0be49
DiamondSi <neighborland@outlook.com>
2017-11-21 18:55:03 +0700

    list

--- singlylinkedlist/src/ru/academits/dubchak/singlylinkedlist/SinglyLinkedList.java
+++ singlylinkedlist/src/ru/academits/dubchak/singlylinkedlist/SinglyLinkedList.java
@@ -9,42 +9,52 @@ public class SinglyLinkedList<T> {
         this.length = 1;
     }
 
-    public ListItem<T> getHead() {
-        return head;
-    }
-
+    //	получение размера списка
     public int getLength() {
         return length;
     }
 
+    //	получение первого узла
+    public ListItem<T> getHead() {
+        return head;
+    }
+
+    //	получение значения по указанному индексу.
     public T getListItemData(int index) {
         return getListItem(index).getData();
     }
 
+    //	изменение значения по указанному индексу.
+    // Изменение значения по индексу пусть выдает старое значение.
     public T setListItemData(int index, T data) {
         T oldData = getListItemData(index);
         getListItem(index).setData(data);
         return oldData;
     }
 
+    //	получение узла по индексу
     public ListItem<T> getListItem(int index) {
         if (index < 0 || index >= length) {
             throw new IllegalArgumentException(Integer.toString(index));
         }
-        ListItem<T> p = head;
-        for (int i = 1; i <= index; i++) {
-            p = p.getNext();
+        int i = 0;
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (i == index) {
+                return p;
+            }
+            ++i;
         }
-        return p;
+        return null;
     }
 
+    // удаление элемента по индексу, пусть выдает значение элемента
     public T delListItem(int index) {
         if (index < 0 || index >= length) {
             throw new IllegalArgumentException(Integer.toString(index));
         }
         T delItemData = getListItemData(index);
         if (index == 0) {
-            head.setNext(getListItem(1));
+            delHead();
         } else if (index == length - 1) {
             getListItem(index).setNext(null);
         } else {
@@ -54,32 +64,41 @@ public class SinglyLinkedList<T> {
         return delItemData;
     }
 
+    //	вставка элемента в начало
     public void insFirstListItem(T data) {
-//        listItem.setNext(getHead().getNext());
-//        getHead().setNext(listItem);
-        ListItem<T> p = new ListItem<T>(data, head);
+        ListItem<T> p = new ListItem<>(data, head);
         length++;
     }
 
-    public void insListItem(ListItem listItem, int index) {
+    //	вставка элемента по индексу
+    public void insListItem(T data, int index) {
         if (index < 0 || index >= length) {
             throw new IllegalArgumentException(Integer.toString(index));
         } else {
-            listItem.setNext(getListItem(index));
-            getListItem(index - 1).setNext(listItem);
+            ListItem<T> p = new ListItem<>(data, getListItem(index));
+            getListItem(index - 1).setNext(p);
             length++;
         }
-
     }
 
-    public void delByValue(int data) {
-        for (int i = 0; i < length; i++) {
-            if (getListItem(i).getData() == data) {
-                int deletedItemIndex = delListItem(i);
-                i--;
+    //TODO удаление узла по значению
+    public void delByValue(T data) {
+        for (ListItem<T> p = head; p != null; p = p.getNext()) {
+            if (p.getData() == data) {
+//                p.setNext(p.getNext());
                 length--;
             }
         }
     }
 
+    //	удаление первого элемента, пусть выдает значение элемента
+    public T delHead() {
+        T data = head.getData();
+        head = head.getNext();
+        return data;
+    }
+
+    //TODO вставка и удаление узла после указанного узла
+    //TODO разворот списка за линейное время
+    //TODO копирование списка
 }
