1a7ee9589e1c96d5f9e40f8f6eb3483868d9ffa2
DiamondSi <neighborland@outlook.com>
2017-11-30 13:38:57 +0700

    list

--- singlylinkedlist/src/ru/academits/dubchak/main/Main.java
+++ singlylinkedlist/src/ru/academits/dubchak/main/Main.java
@@ -6,18 +6,26 @@ import ru.academits.dubchak.singlylinkedlist.SinglyLinkedList;
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList<Integer> list = new SinglyLinkedList(new ListItem(0));
-//        System.out.println(list);
         list.insFirstListItem(1);
         list.insFirstListItem(2);
         list.insFirstListItem(3);
         list.insFirstListItem(4);
-        System.out.println(list);
-        SinglyLinkedList<Integer> list1 = list.copySinglyLinkedList();
-        list.reverseSinglyLinkedList();
-
-//        list.insListItem(1,0);
-
-        System.out.println(list);
-        System.out.println(list1);
+                list.reverseSinglyLinkedList();
+//        System.out.println(list.getLength());
+        System.out.println(list.getHead());
+//        System.out.println(list.getListItemData(0));
+//        System.out.println(list.getListItemData(4));
+//        System.out.println(list.setListItemData(1, 999));
+//        System.out.println(list.getHead());
+        System.out.println(list.getListItem(3));
+//        System.out.printf("list: %s%n", list);
+//        SinglyLinkedList<Integer> list1 = list.copySinglyLinkedList();
+//        System.out.println(list1.getHead());
+//        list.reverseSinglyLinkedList();
+//        System.out.println(list.getHead());
+//        System.out.println(list.getListItemData(0));
+//        System.out.println(list.getListItemData(4));
+//        System.out.printf("list: %s%n", list);
+//        System.out.printf("list1: %s%n", list1);
     }
 }
