43faf81467fabf2fdf299a9f1dc24164408b68ee
DiamondSi <neighborland@outlook.com>
2017-12-18 17:15:59 +0700

    method boolean remove(Object o) done

--- arraylist/src/ru/academits/dubchak/arraylist/MyArrayList.java
+++ arraylist/src/ru/academits/dubchak/arraylist/MyArrayList.java
@@ -100,7 +100,7 @@ public class MyArrayList<E> implements List<E> {
 
     // копируем список в переданный аргументом массив-приемник,
     // если массив-приемник меньше - создаем новый массив нужной длины
-    // если массив-преимник длинее списка, тозаполняем лишние элементы массива значением null TODO не понимаю как это работает, надо протестировать
+    // если массив-приемник длинее списка, тозаполняем лишние элементы массива значением null TODO не понимаю как это работает, надо протестировать
     @Override
     public <E1> E1[] toArray(E1[] array) {
         if (array.length < this.size) {
@@ -114,62 +114,33 @@ public class MyArrayList<E> implements List<E> {
         }
     }
 
-
-    // TODO
-
-    /**
-     * Appends the specified element to the end of this list (optional
-     * operation).
-     * <p>
-     * <p>Lists that support this operation may place limitations on what
-     * elements may be added to this list.  In particular, some
-     * lists will refuse to add null elements, and others will impose
-     * restrictions on the type of elements that may be added.  List
-     * classes should clearly specify in their documentation any restrictions
-     * on what elements may be added.
-     *
-     * @param e element to be appended to this list
-     * @return <tt>true</tt> (as specified by {@link Collection#add})
-     * @throws UnsupportedOperationException if the <tt>add</tt> operation
-     *                                       is not supported by this list
-     * @throws ClassCastException            if the class of the specified element
-     *                                       prevents it from being added to this list
-     * @throws NullPointerException          if the specified element is null and this
-     *                                       list does not permit null elements
-     * @throws IllegalArgumentException      if some property of this element
-     *                                       prevents it from being added to this list
-     */
     @Override
     public boolean add(E e) {
-        return false;
+        if (size >= items.length) {
+            increaseCapacity();
+        }
+        items[size] = e;
+        ++size;
+        ++modCount;
+        return true;
     }
 
-
     // TODO
+    private void increaseCapacity() {
+//        E[] old = items;
+//        items = (E[]) new Object[old.length * 2];
+//        System.arraycopy(old, 0, items, 0, old.length);
+        items = Arrays.copyOf(items, items.length * 2);
+    }
 
-    /**
-     * Removes the first occurrence of the specified element from this list,
-     * if it is present (optional operation).  If this list does not contain
-     * the element, it is unchanged.  More formally, removes the element with
-     * the lowest index <tt>i</tt> such that
-     * <tt>(o==null&nbsp;?&nbsp;get(i)==null&nbsp;:&nbsp;o.equals(get(i)))</tt>
-     * (if such an element exists).  Returns <tt>true</tt> if this list
-     * contained the specified element (or equivalently, if this list changed
-     * as a result of the call).
-     *
-     * @param o element to be removed from this list, if present
-     * @return <tt>true</tt> if this list contained the specified element
-     * @throws ClassCastException            if the type of the specified element
-     *                                       is incompatible with this list
-     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified element is null and this
-     *                                       list does not permit null elements
-     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
-     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
-     *                                       is not supported by this list
-     */
     @Override
     public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(o, items[i])) {
+                this.remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
