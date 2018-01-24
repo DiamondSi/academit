36e3778020302f01f9a41c948796abe056f5085e
DiamondSi <neighborland@outlook.com>
2018-01-24 07:15:00 +0700

    MyArrayList test generated

---
+++ arraylist/src/ru/academits/dubchak/arraylist/MyArrayListTest.java
@@ -0,0 +1,114 @@
+package ru.academits.dubchak.arraylist;
+
+import static org.junit.Assert.*;
+
+public class MyArrayListTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void size() {
+    }
+
+    @org.junit.Test
+    public void isEmpty() {
+    }
+
+    @org.junit.Test
+    public void contains() {
+    }
+
+    @org.junit.Test
+    public void iterator() {
+    }
+
+    @org.junit.Test
+    public void toArray() {
+    }
+
+    @org.junit.Test
+    public void toArray1() {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void remove() {
+    }
+
+    @org.junit.Test
+    public void containsAll() {
+    }
+
+    @org.junit.Test
+    public void addAll() {
+    }
+
+    @org.junit.Test
+    public void addAll1() {
+    }
+
+    @org.junit.Test
+    public void removeAll() {
+    }
+
+    @org.junit.Test
+    public void retainAll() {
+    }
+
+    @org.junit.Test
+    public void clear() {
+    }
+
+    @org.junit.Test
+    public void get() {
+    }
+
+    @org.junit.Test
+    public void set() {
+    }
+
+    @org.junit.Test
+    public void add1() {
+    }
+
+    @org.junit.Test
+    public void remove1() {
+    }
+
+    @org.junit.Test
+    public void indexOf() {
+    }
+
+    @org.junit.Test
+    public void lastIndexOf() {
+    }
+
+    @org.junit.Test
+    public void listIterator() {
+    }
+
+    @org.junit.Test
+    public void listIterator1() {
+    }
+
+    @org.junit.Test
+    public void ensureCapacity() {
+    }
+
+    @org.junit.Test
+    public void trimToSize() {
+    }
+
+    @org.junit.Test
+    public void subList() {
+    }
+}
